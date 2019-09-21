8642ed4465ea8f7d57e5c65ce211310bf258b89f
mikeschubee <mikeschubee11@gmail.com>
2019-09-20 20:34:29 -0400

    Unity Project

---
+++ Robotify/Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    [Header("Camera Follow")]
+    public Transform camera;
+    public float smooth;
+
+    private void Update()
+    {
+        Vector3 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0f);
+
+        transform.position += move;
+
+        Vector3 lookAt = mouseScreenPosition;
+        float AngleRad = Mathf.Atan2(lookAt.y - transform.position.y, lookAt.x - transform.position.x);
+        float AngleDeg = (180 / Mathf.PI) * AngleRad;
+        transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+
+        Vector3 desiredPosition = new Vector3(transform.position.x, transform.position.y, -1f);
+
+        camera.position = desiredPosition;
+
+    }
+}
