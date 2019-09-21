b8aa0b746535a1dc5b36cc162be78c07fb725d5e
Mint16384 <mint16384@gmail.com>
2019-09-21 19:29:25 -0400

    Main Menu waiting for sprites

---
+++ Robotify/Assets/Script/UIFunctions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFunctions : MonoBehaviour
+{
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+    public void PlayButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+}
