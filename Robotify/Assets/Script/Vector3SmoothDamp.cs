78b7c84696f94475ce27c4b01a285c46d73a5eb9
Mint16384 <mint16384@gmail.com>
2019-09-21 22:37:52 -0400

    The camera now scales with the side count! probably not well though...

--- Robotify/Assets/Script/Vector3SmoothDamp.cs
+++ Robotify/Assets/Script/Vector3SmoothDamp.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class Vector3SmoothDamp : MonoBehaviour
 {
     GameObject target;
+    int playerSize;
     float dampTime = .1f;
     Vector3 velocity = Vector3.zero;
     private void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player");
+        playerSize = target.GetComponent<playerAttackManager>().sideCount;
     }
     private void Update()
     {
         if (target)
         {
             transform.position = Vector3.SmoothDamp(transform.position, target.transform.position + new Vector3(0f, 0f, -5f), ref velocity, dampTime);
+        }
+        if(playerSize != target.GetComponent<playerAttackManager>().sideCount)
+        {
+            playerSize = target.GetComponent<playerAttackManager>().sideCount;
+            gameObject.GetComponent<Camera>().orthographicSize = 2 + playerSize;
         }
     }
 }
